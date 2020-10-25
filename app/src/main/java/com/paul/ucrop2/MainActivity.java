380d4858584808fea5ccdd82df3990cc8cbcec0f
zartre <paulpreuss@gmail.com>
2020-10-25 18:52:28 +0100

    Initial commit

---
+++ app/src/main/java/com/paul/ucrop2/MainActivity.java
@@ -0,0 +1,144 @@
+package com.paul.ucrop2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import com.yalantis.ucrop.UCrop;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int CAMERA_ACTION_PICK_REQUEST_CODE = 610;
+    private static final int PICK_IMAGE_GALLERY_REQUEST_CODE = 1;
+
+    private ImageView i;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCamera();
+            }
+        });
+        i = findViewById(R.id.imageView);
+    }
+
+    private void openCamera() {
+        Intent pictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File file;
+        try {
+            file = getImageFile(); // 1
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        Uri uri;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) // 2
+            uri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID.concat(".provider"), file);
+        else
+            uri = Uri.fromFile(file); // 3
+        pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri); // 4
+        startActivityForResult(pictureIntent, 113);
+    }
+
+
+    String currentPhotoPath = "";
+
+    private File getImageFile() throws IOException {
+        String imageFileName = "JPEG_" + System.currentTimeMillis() + "_";
+        File storageDir = new File(
+                Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_DCIM
+                ), "Camera"
+        );
+        System.out.println(storageDir.getAbsolutePath());
+        if (storageDir.exists())
+            System.out.println("File exists");
+        else
+            System.out.println("File not exists");
+        File file = File.createTempFile(
+                imageFileName, ".jpg", storageDir
+        );
+        currentPhotoPath = "file:" + file.getAbsolutePath();
+        return file;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 113 && resultCode == RESULT_OK) {
+            Uri uri = Uri.parse(currentPhotoPath);
+            openCropActivity(uri, uri);
+        } else if (requestCode == UCrop.REQUEST_CROP && resultCode == RESULT_OK) {
+            if (data != null) {
+                Uri uri = UCrop.getOutput(data);
+                    showImage(uri);
+                }
+            }
+        else if (requestCode == PICK_IMAGE_GALLERY_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            try {
+                Uri sourceUri = data.getData();
+                File file = getImageFile();
+                Uri destinationUri = Uri.fromFile(file);
+                openCropActivity(sourceUri, destinationUri);
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    private void openCropActivity(Uri sourceUri, Uri destinationUri) {
+        UCrop.Options options = new UCrop.Options();
+        options.setCircleDimmedLayer(true);
+        options.setCropFrameColor(ContextCompat.getColor(this, R.color.colorAccent));
+        UCrop.of(sourceUri, destinationUri)
+                .withMaxResultSize(600, 800)
+                .withAspectRatio(5f, 5f)
+                .start(this);
+    }
+
+    private void showImage(Uri imageUri) { //Exception {new FileInputStream}, aber muss alle variablen einschließen
+        try {
+            File file;
+            file = getFile(this, imageUri);
+            InputStream inputStream = new FileInputStream(file);
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            i.setImageBitmap(bitmap);
+        }
+        catch (Exception e) {
+            System.out.println("mach etwas mit e");
+            e.printStackTrace();
+        }
+    }
+
+    public File getFile(Context context, Uri uri) {
+
+        String path = uri.getPath();
+        return new File(path);
+    }
+
+}
